51d33e6b2f9c6f695b4a87383823496ae8ad7a25
ana <anaclaracedres@gmail.com>
2025-06-21 04:18:39 -0300

    acciones index: para ver perfil y mis pasajes: para que vea pasajes (falta vista)

---
+++ WebApp/Controllers/Clientes/ClienteController.cs
@@ -0,0 +1,33 @@
+﻿using Dominio;
+using Dominio.Entidades_abstractas;
+using Dominio.Entidades_no_abst;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers.Clientes
+{
+    public class ClienteController : Controller
+    {
+        private Sistema _sistema = Sistema.Instancia;
+        public IActionResult Index()
+        {
+            //---> La siguiente linea es para probar si funciona el controlador ahora que aun no tenemos login (le paso una cedula de un cliente que existe)
+            HttpContext.Session.SetString("cedula", "5291845");
+            //<-----------
+            string cedula = HttpContext.Session.GetString("cedula");
+            Cliente clienteActual = _sistema.BuscarClientePorCedula(cedula);
+
+            if (clienteActual == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            return View(clienteActual);
+        }
+
+        public IActionResult MisPasajes()
+        {
+            return View();
+        }
+    }
+}
+
