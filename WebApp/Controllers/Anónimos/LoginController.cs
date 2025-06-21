0dc0290f8aa59218c962bf0b30a8e62e99122a02
ana <anaclaracedres@gmail.com>
2025-06-21 04:15:54 -0300

    Login y registro: no toque nada

---
+++ WebApp/Controllers/Anónimos/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Dominio;
+using Dominio.Entidades_abstractas;
+using System.Reflection.Metadata;
+
+namespace WebApp.Controllers.Anónimos
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string correo, string contrasenia)
+        {
+            try
+            {
+                if(string.IsNullOrEmpty(correo) || string.IsNullOrEmpty(contrasenia))
+                {
+                    ViewBag.Mensaje = "Email y contraseña son obligatorios.";
+                }
+                else
+                {
+                    
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Mensaje = ex.Message;
+            }
+            return View();
+        }
+    }
+}
---
+++ WebApp/Controllers/Anónimos/RegistroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers.Anónimos
+{
+    public class RegistroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
