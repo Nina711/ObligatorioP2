e63e6c19b4ed140e6ce120174c0cba0f035fd65b
ana <anaclaracedres@gmail.com>
2025-06-21 04:19:17 -0300

    acciones get y post para comprar pasaje

---
+++ WebApp/Controllers/Clientes/PasajeController.cs
@@ -0,0 +1,61 @@
+﻿using Dominio;
+using Dominio.Entidades_no_abst;
+using Dominio.Enumeraciones;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApp.Controllers.Clientes
+{
+    public class PasajeController : Controller
+
+    {
+        private Sistema _sistema = Sistema.Instancia;
+
+        [HttpGet]
+        public IActionResult Comprar(string numVuelo)
+        {
+            Vuelo vuelo = _sistema.BuscarVueloPorId(numVuelo);
+            ViewBag.TiposEquipaje = Enum.GetValues(typeof(Equipaje));
+
+
+            if (vuelo == null)
+            {
+                return RedirectToAction("Index", "Vuelo");
+            }
+
+            return View(vuelo);
+        }
+
+        [HttpPost]
+        public IActionResult Comprar(string numVuelo, DateTime fechaPasaje, Equipaje equipaje)
+        {
+            Vuelo vuelo = _sistema.BuscarVueloPorId(numVuelo);
+
+            //---> La siguiente linea es para probar si funciona el controlador ahora que aun no tenemos login (le paso una cedula de un cliente que existe)
+            HttpContext.Session.SetString("cedula", "5291845");
+            //<-----------
+            string cedula = HttpContext.Session.GetString("cedula");
+            Cliente pasajero = _sistema.BuscarClientePorCedula(cedula);
+
+
+            if (vuelo == null)
+            {
+                return RedirectToAction("Index", "Vuelo");
+            }
+
+            try
+            {
+                decimal precioPasaje = _sistema.CalcularPrecioPasaje(vuelo, pasajero, equipaje);
+                Pasaje p = new Pasaje(vuelo, fechaPasaje, pasajero, equipaje, precioPasaje);
+                _sistema.AgregarPasaje(p);
+                return RedirectToAction("MisPasajes", "Cliente");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Mensaje = ex.Message;
+                ViewBag.TiposEquipaje = Enum.GetValues(typeof(Equipaje));
+                return View(vuelo);
+            }
+        }
+    }
+}
