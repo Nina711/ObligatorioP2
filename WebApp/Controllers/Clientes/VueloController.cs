aaa1dee552123b6f944dfbb306df0879e0dabbe9
ana <anaclaracedres@gmail.com>
2025-06-21 04:18:53 -0300

    lista de vuelos para comprar

---
+++ WebApp/Controllers/Clientes/VueloController.cs
@@ -0,0 +1,16 @@
+﻿using Dominio;
+using Dominio.Entidades_abstractas;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers.Clientes
+{
+    public class VueloController : Controller
+    {
+        private Sistema _sistema = Sistema.Instancia;
+        public IActionResult Index()
+        {
+            ViewBag.Vuelos = _sistema.Vuelos;
+            return View();
+        }
+    }
+}
